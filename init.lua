c4f2d39d17f249e246716f274b68b2a1ab348eb8
jmarkin <me@jmarkin.ru>
2022-01-27 10:14:32 +0300

    fix first start

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
-require('impatient')
+local present, impatient = pcall(require, "impatient")
+if not present then
+   print("Impations not install run :PackerInstall")
+end
 require('settings')
 require('plugins')
