d38036278161fc355aa3d89e9fccb7ae63f9e989
jmarkin <me@jmarkin.ru>
2022-07-09 22:44:02 +0300

    add faster

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -68,6 +68,21 @@ return packer.startup(function()
         end,
     })
 
+    use({
+        "PHSix/faster.nvim",
+        event = { "VimEnter *" },
+        config = function()
+            vim.api.nvim_set_keymap("n", "j", "<Plug>(faster_move_j)", { noremap = false, silent = true })
+            vim.api.nvim_set_keymap("n", "k", "<Plug>(faster_move_k)", { noremap = false, silent = true })
+            -- or
+            -- vim.api.nvim_set_keymap('n', 'j', '<Plug>(faster_move_gj)', {noremap=false, silent=true})
+            -- vim.api.nvim_set_keymap('n', 'k', '<Plug>(faster_move_gk)', {noremap=false, silent=true})
+            -- if you need map in visual mode
+            vim.api.nvim_set_keymap("v", "j", "<Plug>(faster_vmove_j)", { noremap = false, silent = true })
+            vim.api.nvim_set_keymap("v", "k", "<Plug>(faster_vmove_k)", { noremap = false, silent = true })
+        end,
+    })
+
     --- Локальный настройки для папки с помощью .lvimrc с хэшем
     use({
         "klen/nvim-config-local",
