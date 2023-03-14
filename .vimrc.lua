f92af74b52aa61dafebca20113112bded6a850c4
jmarkin <me@jmarkin.ru>
2023-03-14 13:09:17 +0300

    optimize

--- .vimrc.lua
+++ .vimrc.lua
@@ -1,5 +1,7 @@
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
 require("plugins.nullls").enable(nil, nil, nil)
-require("gentags").enable("lua,python")
 vim.g.db = "postgresql://postgres:postgres@localhost:5432/postgres"
--- vim.g.disable_lsp = 1
+
+if vim.fn.executable("ctags") == 1 then
+    require("gentags").enable("lua,python")
+end
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -19,7 +19,7 @@ M.setup = function()
 
         highlight = {
             enable = true,
-            additional_vim_regex_highlighting = syntax,
+            additional_vim_regex_highlighting = true,
             disable = function(_, buf)
                 local max_filesize = 100 * 1024 -- 100 KB
                 local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
