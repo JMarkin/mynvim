910af4f0492174d2c88ce84b9ceba403bea79fb9
jmarkin <me@jmarkin.ru>
2023-03-25 21:28:28 +0300

    change dashboard; change gitui to lazygit; add langmapper

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -95,6 +95,7 @@ require("lazy").setup({
 
     {
         "goolord/alpha-nvim",
+        enabled = false,
         dependencies = {
             {
                 "MaximilianLloyd/ascii.nvim",
@@ -104,6 +105,70 @@ require("lazy").setup({
         config = function()
             require("plugins.dashboard")
         end,
+    },
+
+    {
+        "glepnir/dashboard-nvim",
+        event = "VimEnter",
+        config = function()
+            require("dashboard.theme.header").generate_header = function(config)
+                if not vim.bo[config.bufnr].modifiable then
+                    vim.bo[config.bufnr].modifiable = true
+                end
+                if not config.command then
+                    local header = require("ascii").get_random_global()
+                    vim.api.nvim_buf_set_lines(
+                        config.bufnr,
+                        0,
+                        -1,
+                        false,
+                        require("dashboard.utils").center_align(header)
+                    )
+
+                    for i, _ in ipairs(header) do
+                        vim.api.nvim_buf_add_highlight(config.bufnr, 0, "DashboardHeader", i - 1, 0, -1)
+                    end
+                    return
+                end
+            end
+            require("dashboard").setup({
+                theme = "hyper",
+                config = {
+                    project = { enable = true, limit = 8, icon = " ", label = "", action = ":FzfLua files cwd=" },
+                    shortcut = {
+                        { desc = " Update", group = "@property", action = "Lazy update", key = "u" },
+                        {
+                            icon = " ",
+                            icon_hl = "@variable",
+                            desc = "Files",
+                            group = "Label",
+                            action = "lua require('fzf-lua').files({ multiprocess=true,})",
+                            key = "f",
+                        },
+                        {
+                            icon = " ",
+                            desc = "Recent Files",
+                            group = "DiagnosticHint",
+                            action = "lua require('fzf-lua').oldfiles({ multiprocess = true, cwd_only=true })",
+                            key = "r",
+                        },
+                        {
+                            desc = "  Local Configuration",
+                            group = "Number",
+                            action = ":e .vimrc.lua",
+                            key = "c",
+                        },
+                    },
+                },
+            })
+        end,
+        dependencies = {
+            { "nvim-web-devicons" },
+            {
+                "MaximilianLloyd/ascii.nvim",
+                dependencies = { "MunifTanjim/nui.nvim" },
+            },
+        },
     },
 
     {
@@ -625,6 +690,7 @@ require("lazy").setup({
             require("plugins.fzflua")
         end,
         keys = "<leader>s",
+        cmd = "FzfLua",
     },
     --- Генераци докстрингов и т.п.
     {
@@ -663,6 +729,7 @@ require("lazy").setup({
     -- Классная штука по отображению shortcut
     {
         "folke/which-key.nvim",
+        dependencies = { "Wansmer/langmapper.nvim" },
         config = function()
             require("plugins.whichkey")
         end,
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,6 +1,25 @@
 vim.o.timeout = true
 vim.o.timeoutlen = 300
+
+require("langmapper").setup({})
+require("langmapper").automapping({ global = true, buffer = true })
+local lmu = require("langmapper.utils")
+local view = require("which-key.view")
+local execute = view.execute
+
+-- wrap `execute()` and translate sequence back
+view.execute = function(prefix_i, mode, buf)
+    -- Translate back to English characters
+    prefix_i = lmu.translate_keycode(prefix_i, "default", "ru")
+    execute(prefix_i, mode, buf)
+end
+
+local presets = require("which-key.plugins.presets")
+presets.objects = lmu.trans_dict(presets.objects)
+presets.motions = lmu.trans_dict(presets.motions)
+
 local wk = require("which-key")
+
 wk.setup({
     icons = {
         breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
