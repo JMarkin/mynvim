4725bbd720cce3683ef9b65d7b269a1890dba2ba
jmarkin <me@jmarkin.ru>
2023-05-09 14:49:20 +0300

    nvim-dap

---
+++ lua/plugins/pairs.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+M.plugin = {
+    "altermo/npairs-integrate-upair",
+    dependencies = {
+        "hrsh7th/nvim-cmp",
+        "windwp/nvim-autopairs",
+        "altermo/ultimate-autopair.nvim",
+    },
+    event = { "InsertEnter", "CmdlineEnter" },
+    config = function()
+        require("npairs-int-upair").setup({ map = "u" })
+        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        local cmp = require("cmp")
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+}
+
+return M
