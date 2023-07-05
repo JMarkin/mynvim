80f475183d553a294c1b9da6d870333c8bcc87e9
jmarkin <me@jmarkin.ru>
2023-07-05 21:16:51 +0300

    complete flash nvim

---
+++ lua/plugins/db.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+local is_not_mini = require("custom.funcs").is_not_mini
+
+local dbui = {
+    "kristijanhusak/vim-dadbod-ui",
+    enabled = true,
+    cond = is_not_mini,
+    dependencies = {
+        "tpope/vim-dadbod",
+        {
+            "kristijanhusak/vim-dadbod-completion",
+            dependencies = "nvim-cmp",
+        },
+    },
+    init = function()
+        vim.g.db_ui_execute_on_save = 0
+        vim.g.db_ui_win_position = "right"
+        vim.g.db_ui_show_database_icon = 1
+        vim.g.db_ui_use_nerd_fonts = 1
+    end,
+    ft = { "sql", "mssql", "plsql" },
+    cmd = { "DB", "DBUI" },
+}
+
+M.plugin = dbui
+
+return M
