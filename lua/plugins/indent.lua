077f2930a765af4e023aae72835dbd5ffe21765a
jmarkin <me@jmarkin.ru>
2023-10-15 20:38:22 +0300

    up plugins

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,4 +1,4 @@
-vim.opt.list = true
+-- vim.opt.list = true
 -- vim.opt.listchars:append("space:⋅")
 -- vim.opt.listchars:append("eol:↴")
 --
@@ -7,6 +7,11 @@ local is_not_mini = require("funcs").is_not_mini
 return {
     "lukas-reineke/indent-blankline.nvim",
     cond = is_not_mini,
-    lazy = true,
-    opts = {},
+    event = "VeryLazy",
+    config = function()
+        local hooks = require("ibl.hooks")
+
+        require("ibl").setup({ scope = { highlight = vim.g.rainbow_delimiters_highlight } })
+        hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+    end,
 }
