304c3905cfbd9c672de3648229d9fb388eaa8776
jmarkin <me@jmarkin.ru>
2023-12-29 10:54:36 +0000

    temp fix neowords

--- lua/plugins/neowords.lua
+++ lua/plugins/neowords.lua
@@ -10,17 +10,20 @@ return {
     },
     config = function()
         local neowords = require("neowords")
-        local p = neowords.pattern_presets
+        -- local p = neowords.pattern_presets
 
-        local subword_hops = neowords.get_word_hops(
-            p.sneak_case,
-            p.camel_case,
-            p.upper_case,
-            p.number,
-            p.hex_color,
-            "\\v#+",
-            "\\v[<>]+"
-        )
+        local p = {
+            snake_case = "\\v[[:lower:]]+",
+            camel_case = "\\v[[:upper:]][[:lower:]]+",
+            upper_case = "\\v[[:upper:]]+[[:lower:]]@!",
+            number = "\\v[[:digit:]]+",
+            hex_color = "\\v#[[:xdigit:]]+",
+
+            any_word = "\\v-@![-_[:lower:][:upper:]]+",
+        }
+
+        local subword_hops =
+            neowords.get_word_hops(p.sneak_case, p.camel_case, p.upper_case, p.number, p.hex_color, "\\v#+", "\\v[<>]+")
 
         vim.keymap.set({ "n", "x", "o" }, "w", subword_hops.forward_start)
         vim.keymap.set({ "n", "x", "o" }, "e", subword_hops.forward_end)
