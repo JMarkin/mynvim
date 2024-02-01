7e20d815ee85f45fe59607cecebce41d8ad90037
jmarkin <me@jmarkin.ru>
2024-02-02 00:18:46 +0300

    fix neovide

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -4,33 +4,17 @@ vim.g.neovide_window_blurred = true
 vim.g.neovide_remember_window_size = false
 vim.g.neovide_input_use_logo = 1
 
-vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
-vim.keymap.set("v", "<D-c>", '"+y') -- Copy
-vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
-vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
-vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
-vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
-
-vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("!", "<D-v>", "<C-v>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("t", "<D-v>", "<C-v>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<D-v>", "<C-v>", { noremap = true, silent = true })
-
-vim.g.neovide_scale_factor = 1.0
-local change_scale_factor = function(delta)
-    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
-end
-vim.keymap.set("n", "<C-+>", function()
-    change_scale_factor(1.25)
-end)
-vim.keymap.set("n", "<C-->", function()
-    change_scale_factor(1 / 1.25)
-end)
-
+-- stylua: ignore start
 vim.cmd([[
+    cmap <D-v> <C-r>+
+    map <D-v> "+p<CR>
+    map! <D-v> <C-R>+
+    tmap <D-v> <C-v>
+    vmap <D-c> "+y<CR>
+
     map ˙ <a-h>
     map ∆ <a-j>
-    map ˚ <a-k
+    map ˚ <a-k>
     map ¬ <a-l>
     map ƒ <a-f>
     map © <a-g>
@@ -42,3 +26,16 @@ vim.cmd([[
     map ˆ <a-i>
     tmap ˆ <a-i>
 ]])
+-- stylua: ignore end
+
+vim.g.neovide_scale_factor = 1.0
+local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + delta
+    vim.cmd(":redraw!")
+end
+vim.keymap.set("n", "<C-=>", function()
+    change_scale_factor(0.25)
+end)
+vim.keymap.set("n", "<C-->", function()
+    change_scale_factor(-0.25)
+end)
