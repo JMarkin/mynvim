944cae133f3cdfb8aaa12c6e621dc98bc1ea97c8
jmarkin <me@jmarkin.ru>
2024-07-14 14:44:08 +0300

    up lua_ls

--- installers/luals.sh
+++ installers/luals.sh
@@ -2,11 +2,11 @@
 
 cd /opt
 rm -rf lua-language-server
-git clone -b 3.7.4 https://github.com/LuaLS/lua-language-server
+git clone -b 3.9.3 https://github.com/LuaLS/lua-language-server
 cd lua-language-server
 ./make.sh
 mkdir -p ~/.local/bin
 cd ~/.local/bin
 rm -f lua-language-server
-wget https://gist.githubusercontent.com/JMarkin/b6db04a6660bf8b65cd02937057fccc0/raw/2d2a0a2977e373d43b8ae3c7a30a82f88b83464b/lua-language-server
+curl -o lua-language-server https://gist.githubusercontent.com/JMarkin/b6db04a6660bf8b65cd02937057fccc0/raw/2d2a0a2977e373d43b8ae3c7a30a82f88b83464b/lua-language-server
 chmod +x ./lua-language-server
