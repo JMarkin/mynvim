6de23dc60739ea0ef2b28c3f5a62502f0f32e7a2
jmarkin <me@jmarkin.ru>
2024-07-23 15:33:32 +0300

    disable luminate, fix pip plugins

--- installers/pip.sh
+++ installers/pip.sh
@@ -3,21 +3,22 @@
 NAME=$1
 PIP_ARGS=$2
 
-mkdir -p ${HOME}/.local/bin
-mkdir -p ${HOME}/.local/share/py
+mkdir -p "${HOME}"/.local/bin
+mkdir -p "${HOME}"/.local/share/py
 
 VENV="${HOME}/.local/share/py/${NAME}"
 BIN="${HOME}/.local/bin/${NAME}"
 
 echo "remove previous ${BIN} ${VENV}"
-rm -rf ${BIN} ${VENV}
+rm -rf "${BIN}" "${VENV}"
 
 echo "create venv ${VENV}"
-python -m virtualenv ${VENV}
-ls ${HOME}/.local/share/py
+python -m venv --copies "${VENV}"
+#ls "${HOME}"/.local/share/py
 
 echo "install ${NAME}"
-${VENV}/bin/pip install -U ${NAME}
+"${VENV}"/bin/pip install -U pip
+"${VENV}"/bin/pip install -U "${NAME}" "${PIP_ARGS}"
 
 echo "link binary ${VENV}/bin/${NAME} -> ${BIN}"
 ln "${VENV}/bin/${NAME}" "${BIN}"
